ac7134e5371e581c75f051ebd2da22643bcda369
mohamedgika <dodm773@gmail.com>
2022-09-28 16:58:28 +0300

    first commit

---
+++ calculator.php
@@ -0,0 +1,37 @@
+<?php
+
+class calculator{
+
+    public function add($x,$y){
+        return $x+$y;
+    }
+
+    public function sub($x,$y){
+        return $x-$y;
+    }
+
+    public function div($x,$y){
+        return $x/$y;
+    }
+
+    public function multi($x,$y){
+        return $x*$y;
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ tests/calculatorTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+require "calculator.php";
+
+class calculatorTest extends TestCase
+{
+
+    // public function test_equal_true()
+    // {
+    //     $this->assertTrue(true);
+    // }
+
+    public function test_add_calc()
+    {
+        $c = new calculator();
+        $this->assertEquals( $c->add(1,1) , 2);  // Test Function And Put Suggest Number
+    }
+
+    
+    public function test_sub_calc()
+    {
+        $c = new calculator();
+        $this->assertEquals( $c->sub(3,2) , 1);  
+    }
+
+    
+    public function test_multi_calc()
+    {
+        $c = new calculator();
+        $this->assertEquals( $c->multi(5,5) , 25);
+    }
+
+    
+    public function test_div_calc()
+    {
+        $c = new calculator();
+        $this->assertEquals( $c->div(10,2) , 5);     
+    }
+}
+
+
+
+
+
+
+
+
+
+?>
